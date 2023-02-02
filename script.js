8d26d30c553b8b2bb57d23a0312f011d1a52d7cd
Ryan Cheung <ryan_cheung@hotmail.co.uk>
2023-02-02 23:07:04 +0000

    Date added in hero. Cuurent hour variable and selectors created in JS

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
+// Current Time
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
+
+// Current hour
+
+var currentHour = moment().hour()
+// console.log(currentHour);
+
+// 9-5 hour selectors
+
+var timeBlocks = $(".time-block")
+// console.log(timeBlocks);
+
+// Text Area selectors
+
+
+
+// Colour Coding Section //
+
+// function colourCode() {
+// if
+// }
